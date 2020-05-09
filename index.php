5abe28d068d3f70cac93f48d117b5e72345a10b5
calaw1110 <calaw1110@gmail.com>
2020-05-09 17:17:15 +0800

    no message

--- index.php
+++ index.php
@@ -31,7 +31,9 @@ if (isset($_SESSION['userLoggedIn'])) {
           <a href="index.php" class="logo"><img src="assets/images/icons/logo.png" alt=""></a>
           <div class="group">
             <div class="navItem">
-              <a href="search.php" class="navItemLink">Search</a>
+              <a href="search.php" class="navItemLink">Search
+                <img src="assets/images/icons/search.png" alt="search" class="icon">
+              </a>
             </div>
           </div>
           <div class="group">
