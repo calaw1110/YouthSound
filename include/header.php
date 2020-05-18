27c620b2a5d9ac471501ce8f656d8b78502768cd
calaw1110 <calaw1110@gmail.com>
2020-05-18 15:06:00 +0800

    <BUG> php 程式 放在最外圍時 會造成畫面向下推擠，放進html的標籤裡就不會

--- include/header.php
+++ include/header.php
@@ -1,3 +1,17 @@
+<?php
+include("include/config.php");
+//要先include Artist class 才能 include Album class
+include("include/classes/Artist.php"); //1
+include("include/classes/Album.php"); //2
+include("include/classes/Song.php"); //3
+include("include/classes/Genre.php"); //4
+//session_destroy();LOG OUT
+//假如有登入狀態
+if (isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+    header("Location: Account.php");
+} ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,25 +19,12 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
     <link rel="stylesheet" href="assets/css/style.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
     <script src="assets/js/script.js"></script>
-    <?php
-    include("include/config.php");
-    //要先include Artist class 才能 include Album class
-    include("include/classes/Artist.php"); //1
-    include("include/classes/Album.php"); //2
-    include("include/classes/Song.php"); //3
-    //session_destroy();LOG OUT
-    //假如有登入狀態
-    if (isset($_SESSION['userLoggedIn'])) {
-        $userLoggedIn = $_SESSION['userLoggedIn'];
-        //取得已登入的使用者名稱
-        echo "<script>userLoggedIn='$userLoggedIn';</script>";
-    } else {
-        header("Location: Account.php");
-    } ?>
     <title>YouthSound</title>
+
 </head>
 
 <body>
