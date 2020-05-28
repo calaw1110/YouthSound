06cc28681a8a774990ac67f05bb243d0929ae5d9
calaw1110 <calaw1110@gmail.com>
2020-05-29 01:06:56 +0800

    新增歌單頁面

--- includes/classes/Playlist.php
+++ includes/classes/Playlist.php
@@ -7,6 +7,13 @@ class Playlist
     private $owner;
     public function __construct($conn, $data)
     {
+        //if $data is an id (string)
+        if (!is_array($data)) {
+            //用id去取得歌單資訊
+            $DataQuery = mysqli_query($conn, "SELECT * FROM playlists WHERE id='$data'");
+            //回傳資料陣列
+            $data = mysqli_fetch_array($DataQuery);
+        }
         $this->conn = $conn;
         $this->id = $data['id'];
         $this->name = $data['name'];
@@ -23,5 +30,21 @@ class Playlist
     public function getOwner()
     {
         return $this->owner;
+    }
+    //取得歌單裡面有多少首歌曲
+    public function getNumberOfSongs()
+    {
+        $query = mysqli_query($this->conn, "SELECT songId FROM playlistSongs WHERE playlistId ='$this->id'");
+        return mysqli_num_rows($query);
+    }
+    public function getSongIds()
+    {
+        $query = mysqli_query($this->conn, "SELECT songId FROM playlistSongs
+        WHERE playlistId='$this->id' ORDER BY playlistOrder ASC");
+        $array = array();
+        while ($row = mysqli_fetch_array($query)) {
+            array_push($array, $row['songId']);
+        }
+        return $array;
     }
 }
