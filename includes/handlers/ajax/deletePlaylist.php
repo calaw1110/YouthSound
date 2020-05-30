7be3f9650b29f548041bec6bc9b7f420fad5ae0b
calaw1110 <calaw1110@gmail.com>
2020-05-30 17:17:18 +0800

    更新優先順序 先刪除歌單內的歌曲再刪除歌單

--- includes/handlers/ajax/deletePlaylist.php
+++ includes/handlers/ajax/deletePlaylist.php
@@ -3,11 +3,15 @@ include "../../config.php";
 
 if (isset($_POST['playlistId'])) {
     $playlistId = $_POST['playlistId'];
-    $deletePlaylistsQuery = mysqli_query($conn,
-        "DELETE FROM playlists WHERE id='$playlistId'"
-    );
-    $deletePlaylistSongsQuery = mysqli_query($conn,
+    //刪除歌單內的歌曲
+    $deletePlaylistSongsQuery = mysqli_query(
+        $conn,
         "DELETE FROM playlistsongs WHERE playlistid='$playlistId'"
+    );
+    //刪除歌單
+    $deletePlaylistsQuery = mysqli_query(
+        $conn,
+        "DELETE FROM playlists WHERE id='$playlistId'"
     );
 } else {
     echo "刪除失敗";
