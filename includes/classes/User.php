de8c32c02f2b7109d6527ff56bf9089c4224f36c
calaw1110 <calaw1110@gmail.com>
2020-05-31 18:01:57 +0800

    userDetail create

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -12,4 +12,10 @@ class User
     {
         return $this->username;
     }
+    public function getUserNickname()
+    {
+        $nicknameQuery = mysqli_query($this->conn, "SELECT mNickname FROM members  WHERE mUsername ='$this->username'");
+        $row = mysqli_fetch_array($nicknameQuery);
+        return $row['mNickname'];
+    }
 }
