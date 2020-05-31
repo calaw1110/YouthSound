c761a6aa076bae4069305c58f2d90e76a9d18293
calaw1110 <calaw1110@gmail.com>
2020-06-01 02:23:30 +0800

    新增修改功能的function 以及 功能AJAX

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,23 @@ $(document).on("change","select.playlist",function(){
         select.val("");
     });
 })
+function updateEmail(emailClass){
+    var emailValue=$("."+emailClass).val();
+    alert(emailValue);
+    $.post("includes/handlers/ajax/updateEmail.php",{email:emailValue,username:userLoggedIn}).done(function(response){
+        $("."+emailClass).nextAll(".message").text(response);
+    })
+}
+function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2){
+    var oldPassword = $("." + oldPasswordClass).val();
+    var newPassword1 = $("." + newPasswordClass1).val();
+    var newPassword2 = $("." + newPasswordClass2).val();
+    alert(oldPassword +',' +newPassword1 +', '+newPassword2);
+    $.post("includes/handlers/ajax/updatePassword.php", { oldPassword: oldPassword, newPassword1: newPassword1, newPassword2: newPassword2, username: userLoggedIn}).done(function(response){
+        $("." + oldPasswordClass).nextAll(".message").text(response);
+    })
+}
+
 function logout() {
     $.post("includes/handlers/ajax/logout.php",function(){
         location.reload();
